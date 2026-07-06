0a0e68597a31bc185c492cde8a879a28d40e3b35
= <=>
2026-07-06 14:46:28 +0530

    salary

---
+++ Lec3/bsalary.py
@@ -0,0 +1,6 @@
+bs=float(input("Enter salary: "))
+hra=float(input("Enter hra: "))
+ba=float(input("Enter ba: "))
+pf=float(input("Enter pf: "))
+total=bs+hra+ba-pf
+print(f"the net salary is: {total}")
