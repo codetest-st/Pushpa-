1bae003255ff3459e94479f674c23385f684c12f
= <=>
2026-07-06 14:52:53 +0530

    price

---
+++ Lec3/productprice.py
@@ -0,0 +1,6 @@
+tpen=12
+totalpriceofpen=120
+wpen=int(input("Enter amount your want: "))
+priceperpen=totalpriceofpen/tpen
+wantedprice=priceperpen*wpen
+print(f"the price of {wpen} pen is: {wantedprice}")
