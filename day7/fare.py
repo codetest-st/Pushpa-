91d64e3a0d50acba8bcc33c74a0ff9f57e1cd19d
= <=>
2026-07-09 14:46:47 +0530

    bookdis,eponentopt,fare,ptr

---
+++ day7/fare.py
@@ -0,0 +1,8 @@
+children=int(input("enter number of children:"))
+youngs=int(input("enter number of youngs:"))
+olds=int(input("enter number of olds:"))
+fare_children= children*450
+fare_youngs= youngs*650
+fare_olds= olds*500
+total_fare = fare_children + fare_youngs + fare_olds
+print(f"the fare of child is {fare_children}\nthe fare of youngs is{fare_youngs}\nthe fare of old {fare_olds}\nthe total fare is{total_fare}")
